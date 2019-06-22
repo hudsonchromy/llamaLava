343e1b61e66240cdd64f99bc7fd4c4e3e7eda7e6
hudsonchromy <hjc0018@auburn.edu>
2019-06-22 15:26:57 -0500

    added debugging

--- highscores.php
+++ highscores.php
@@ -3,7 +3,7 @@
 	$score = "";
 	$errors = array();
 
-	$db = mysqli_connect('127.0.0.1', 'root', '12345', 'lavaLlama');
+	$db = mysqli_connect('mysql://bddc45f25fedbd:23c2eedf@us-cdbr-iron-east-02.cleardb.net/heroku_7291367ba0c4da0?reconnect=true', 'root', '12345', 'lavaLlama');
 	$query = "SELECT * FROM scores ORDER BY score DESC LIMIT 5";
 	$results = mysqli_query($db, $query);
 	if ($results->num_rows > 0) {
@@ -20,15 +20,15 @@
 	if (isset($_POST['submit']) && !empty($initials)) {
 		$sql = "INSERT INTO scores (score, initials) VALUES ($s, '$initials')";
 		$result = mysqli_query($db, $sql);
-		// if ($result) {
-		// 	echo "$sql";
-		// 	echo "</br>";
-		// 	echo "Success";
-		// }
-		// else {
-		// 	echo "$sql";
-		// 	echo "</br>";
-		// 	echo "Failure";
-		// }
+		if ($result) {
+			echo "$sql";
+			echo "</br>";
+			echo "Success";
+		}
+		else {
+			echo "$sql";
+			echo "</br>";
+			echo "Failure";
+		}
 	}
 ?>
